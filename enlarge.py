cf03a75c59e3b49f4914c3fd751c415f8abb7658
OMalenfantThuot <malenfantthuotolivier@gmail.com>
2018-08-20 11:55:33 -0400

    enlarge.py executable

---
+++ enlarge.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import functions_briaree as fbr
+
+"""
+Script pour agrandir les fichiers positions .ascii du graphène
+S'appelle comme un éxécutable
+
+"""
+infile = str(input('Entrer le nom du fichier à agrandir:\n'))
+
+Pos = fbr.Posinp()
+Pos.def_from_file(infile)
+outfile = 'out_'+infile.strip('.' + Pos.filetype)
+
+xsize = int(input('Nouvelle taille en x:\n'))
+zsize = int(input('Nouvelle taille en z:\n'))
+
+Pos.enlarge_graphene_cell([xsize,zsize])
+Pos.create_file(outfile,increment='True')
